77ef185f4ce90ef947ab02d24db8d441245da822
StephenTse <stephenyctsedev@gmail.com>
2022-10-11 21:18:27 -0500

    save

--- autoClicker.py
+++ autoClicker.py
@@ -2,13 +2,9 @@ import pyautogui
 import time
 import random
 
-for z in range(1,10):
-    x = random.randint(0,500)
-    y = random.randint(0,500)
-    pyautogui.moveTo(x,y,2)
+for z in range(100):
+    pyautogui.press('num6')
 
     localtime = time.localtime()
     result = time.strftime("%I:%M:%S %p", localtime)
-
-    print('Moved at ' + str(result) + '(' + str(x) + ', ' + str(y) + ')')
-    time.sleep(2)
+    print(str(z) + ', ' + str(result))
